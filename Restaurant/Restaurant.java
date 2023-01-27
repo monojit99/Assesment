7bc437498e25b60ca639c3266806c846b877e57a
MONOJIT CHOWDHURY <55353767+monojit99@users.noreply.github.com>
2023-01-27 14:30:43 +0530

    Add files via upload

---
+++ Restaurant/Restaurant.java
@@ -0,0 +1,34 @@
+package basicprograms;
+
+import java.util.Scanner;
+//Restrurant class
+public class Restaurant {
+	
+//main method
+	public static void main(String[] args) {
+		System.out.println("******************************WelCome to Restaurant management System********************");
+		SelectMenu sm=new SelectMenu();
+		char ch;
+		sm.displaymenu();//invoke the display menu function
+		//sm.selectmenu();
+		Scanner sc=new Scanner(System.in);
+		do {
+			//ask user for order food or not
+		System.out.println("do you want to order(y/n)?");
+		ch=sc.next().charAt(0);
+		switch(ch)
+		{
+		case 'y':
+			sm.selectmenu(); //invoke selectmenu method
+			break;
+		case 'n':
+		sm.bill();// invoke bill method
+		break;
+		}
+		}while(ch!='n');
+		
+		
+		
+	}
+
+}
